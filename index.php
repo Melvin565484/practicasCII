29192d726594c786eab58230490ac9821567b3d6
Melvin <melvinsantiago396@gmail.com>
2023-03-06 21:25:46 -0300

    practica1

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+    //echo phpinfo();
+    $variable = "PROGRAMACION 4°";
+    $entero = 20;
+
+    var_dump($variable);
+
+    //CASTING
+    $variable = (bool) $variable;
+    var_dump($variable)
+
+
+    //??
+    $a = null;
+    $b = 40;
+    $c = "valor;"
+
+    //echo $z ?? $a  ?? $b ?? $c;
+
+    //operador  nave espacial
+    //<=>
+    echo  10<=>10;
+
+?>
